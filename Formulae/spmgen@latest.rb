ab417298076c1fe82d184e2040099d14453b24ed
maximkrouk <id.maximkrouk@gmail.com>
2021-05-07 14:08:41 +0300

    Initial commit

---
+++ Formulae/spmgen@latest.rb
@@ -0,0 +1,17 @@
+class SPMGenATLatest < Formula
+    desc "`spmgen`is a resources boilerplate code generator for Swift"
+    homepage "https://github.com/makeupstudio/spmgen"
+    url "https://github.com/makeupstudio/spmgen.git", :branch => "main"
+    version "latest"
+
+    depends_on :xcode => ["12.4", :build]
+
+    def install
+        system "swift build -c release --disable-sandbox --build-path '.build'"
+        bin.install ".build/release/spmgen"
+    end
+
+    test do
+        system "#{bin}/spmgen --version"
+    end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# Makeupstudio Formulae
+
+## Setup
+
+```bash
+brew tap makeupstudio/formulae
+```
+
+> Using SSH instead of HTTP:
+
+```bash
+brew tap makeupstudio/formulae git@github.com:makeupstudio/homebrew-formulae.git
+```
+
+## Available Taps
+
+### [SPMGen](https://github.com/makeupstudio/spmgen)
+
+```bash
+brew install makeupstudio/formulae/spmgen
+```
