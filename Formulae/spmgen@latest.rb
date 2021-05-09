fb98406e8161d2e46b1e294ddc68ad56969a381c
maximkrouk <id.maximkrouk@gmail.com>
2021-05-09 22:50:28 +0300

    clean: homebrew warnings

--- Formulae/spmgen@latest.rb
+++ Formulae/spmgen@latest.rb
@@ -1,17 +1,17 @@
 class SPMGen < Formula
-    desc "Resources boilerplate code generator for Swift"
-    homepage "https://github.com/makeupstudio/spmgen"
-    url "https://github.com/makeupstudio/spmgen.git", :branch => "main"
-    version "latest"
+  desc "Resources boilerplate code generator for Swift"
+  homepage "https://github.com/makeupstudio/spmgen"
+  url "https://github.com/makeupstudio/spmgen.git", :branch => "main"
+  version "latest"
 
-    depends_on :xcode => ["12.4", :build]
+  depends_on xcode: ["12.4", :build]
 
-    def install
-        system "swift",  "build", "-c", "release", "--disable-sandbox", "--build-path", "'.build'"
-        bin.install ".build/release/spmgen"
-    end
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox", "--build-path", "'.build'"
+    bin.install ".build/release/spmgen"
+  end
 
-    test do
-        system "#{bin}/spmgen", "--version"
-    end
+  test do
+    system "#{bin}/spmgen", "--version"
+  end
 end
