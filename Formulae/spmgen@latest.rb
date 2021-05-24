85e479c7790c3468b80c8dbeec249babbcfa303a
maximkrouk <id.maximkrouk@gmail.com>
2021-05-24 15:04:27 +0300

    fix: classname & indentation for spmgen@latest

--- Formulae/spmgen@latest.rb
+++ Formulae/spmgen@latest.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-class Spmgen@latest < Formula
+class SpmgenAtLatest < Formula
   desc "Resources boilerplate code generator for Swift"
   homepage "https://github.com/makeupstudio/spmgen"
   url "https://github.com/makeupstudio/spmgen.git", branch: "main"
   version "latest"
-
+  
   depends_on xcode: ["12.4", :build]
-
+  
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox", "--build-path", "'.build'"
     bin.install ".build/release/spmgen"
   end
-
+  
   test do
     system "#{bin}/spmgen", "--version"
   end
